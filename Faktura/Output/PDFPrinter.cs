a3c2bd06f3a0a63b33b0be6f3870eb74c4a4b501
Hubert Kuszyński <hkuszynski@gmail.com>
2019-08-14 12:23:07 +0200

    GOGOGO

---
+++ Faktura/Output/PDFPrinter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktura.Output
+{
+    public class PDFPrinter
+    {
+    }
+}
